570f3fbc1f3dc70f5b50332012a74a82e2c587c0
Abner <abnercruz4@hotmail.com>
2025-12-07 18:31:16 -0300

    interactive server

--- obj/Debug/net9.0/FrontEnd.AssemblyInfo.cs
+++ obj/Debug/net9.0/FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66bf553ebce675334d159c5b963796235de286db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad3517401189fe4f801f8255fae9ce6f1fc399c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
