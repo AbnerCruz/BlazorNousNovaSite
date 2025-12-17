aa9e187ba8ac3e0c6e38dd9042db6a8df8e322c3
Abner <abnercruz4@hotmail.com>
2025-12-17 16:08:12 -0300

    Setting te repository

--- obj/Debug/net9.0/FrontEnd.AssemblyInfo.cs
+++ obj/Debug/net9.0/FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b2add4a4c633a9945ae6fe7a7e757909d80e2c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9ddacfc4e08d685057ed5f892339b8b108b548")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
